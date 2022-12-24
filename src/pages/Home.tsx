328a294f38a8404d9bc4b47bd03e7784ab322d1f
unknown <abisesh123@gmail.com>
2022-12-24 20:10:10 +0545

    makes todo editable and deleteable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import React, {useEffect,useState} from 'react'
 import axios from "axios";
 
-
+interface ITodo{
+    id:number;
+    message:string;
+}
 const Home = () => {
-    const [data,setData]=useState([])
+    const [data,setData]=useState([] as ITodo[])
+    const [editedTodo,setEditedTodo]=useState({} as ITodo)
     const [message,setMessage]=useState("" as string)
 
     const fetchData=async () => {
@@ -16,23 +20,63 @@ const Home = () => {
         setMessage(event.target.value)
     }
 
+    const handleEditChange=(event:any)=>{
+        event.persist();
+        setEditedTodo({
+            ...editedTodo,
+            message:event.target.value,
+        })
+    }
+
     const handleSubmit=async()=>{
         let respnose=await axios.post("http://localhost:4000/todos",{
             message:message,
         });
         fetchData()
     }
+    
+
+    const handleTodoEdit=async()=>{
+        let response=await axios.patch(
+            `http://localhost:4000/todos/${editedTodo.id}`,
+            {
+                message:editedTodo.message,
+            }
+        );
+        fetchData();
+    }
+
+    const handleCardEdit=async(id:number)=>{
+        let response=await axios.get(
+            `http://localhost:4000/todos/${id}`);
+            setEditedTodo(response.data);
+    }
+
+    const deleteTodo=async(id:number)=>{
+        let response=await axios.delete( `http://localhost:4000/todos/${id}`);
+        fetchData();
+    }
+
     useEffect(() => {
         fetchData();
     }, []);
-    
+
+
+        
   return (
+    
     <div className='main-wrapper'>
     
         <div className='card-wrapper'>
             {data.map((message:any)=>(
                 <div className='card'>
-                    <p className='card-message'>{message.message}</p>
+                    <p onClick={()=>{
+                        handleCardEdit(message.id)
+                    }} className="card-message">{message.message}</p>
+                
+                <div>
+                <button onClick={()=>{deleteTodo(message.id)}}>D</button>
+                </div>
                 </div>
             ))}
         </div>
@@ -43,11 +87,22 @@ const Home = () => {
             
                 id="message"
                 type="text"/>
+            <button onClick={handleSubmit}>Add Message</button>
 
         </div>
 
-    </div>
-  )
+        <div className='input-wrapper'>
+            <input
+                value={editedTodo.message}
+                onChange={handleEditChange}
+            
+                id="message"
+                type="text"/>
+            <button onClick={handleTodoEdit}>Edit Message</button>
+
+        </div>
+        </div>
+ )
 }
 
-export default Home
+export default Home;
